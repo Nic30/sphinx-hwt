49a0a59e4c3432fc3ce305043a9270f24af632b4
Nic30 <nic30@seznam.cz>
2018-07-21 12:30:13 +0200

    properly reload modules in tests

--- sphinx_hwt/tests/test.py
+++ sphinx_hwt/tests/test.py
@@ -5,13 +5,15 @@ from shutil import rmtree
 from sphinx.cmdline import main as sphinx_main
 from sphinx.ext.apidoc import main as apidoc_main
 import sys
-from unittest.mock import patch
+from types import ModuleType
 import unittest
+from unittest.mock import patch
+
 
 pwd = os.path.dirname(path.realpath(__file__))
 
 # allow to use this extension
-sys.path.append("..")
+sys.path.append(path.join(pwd, ".."))
 
 
 def assert_err_on_exit(err):
@@ -24,31 +26,31 @@ def assert_err_on_exit(err):
 def _run_test(mock_stdout):
     rmtree("doc/", ignore_errors=True)
     rmtree("doc_buld/", ignore_errors=True)
-    
+
     # [OPTIONS] -o <OUTPUT_PATH> <MODULE_PATH> [EXCLUDE_PATTERN, ...]
     # apidoc_main(["-h"])
     ret = apidoc_main(["--module-first", "--force", "--full",
                        "--output-dir", "doc/", "."])
-    
+
     if ret != 0:
         sys.stderr.flush()
         if mock_stdout is not None:
             sys.stdout.flush()
- 
+
         raise AssertionError("apidoc_main failed with err %d" % ret)
-    
+
     # -b buildername
     # -a If given, always write all output files. The default is to only write output files for new and changed source files. (This may not apply to all builders.)
     # -E Don’t use a saved environment (the structure caching all cross-references), but rebuild it completely. The default is to only read and parse source files that are new or have changed since the last run.
     # -C Don’t look for a configuration file; only take options via the -D option.
     # [OPTIONS] SOURCEDIR OUTPUTDIR [FILENAMES...]
-    
+
     # sphinx_main(["-h"])
     ret = sphinx_main(["-b", "html", "-E",
-          "-c", pwd,
-          "doc/",
-          "doc_build/",
-    ])
+                       "-c", pwd,
+                       "doc/",
+                       "doc_build/",
+                       ])
     if ret != 0:
         sys.stderr.flush()
         if mock_stdout is not None:
@@ -66,11 +68,16 @@ def run_test(name):
         os.chdir(name)
         # allow test modules import
         sys.path.append(".")
-        
+
         return _run_test(None)
     finally:
         os.chdir(pwd)
         sys.path.pop()
+        for name, m in list(sys.modules.items()):
+            if isinstance(m, ModuleType) \
+                    and hasattr(m, "__file__") \
+                    and m.__file__.startswith("./"):
+                del sys.modules[name]
 
 
 class HwtSchematic_directive_TC(unittest.TestCase):
@@ -83,7 +90,7 @@ class HwtSchematic_directive_TC(unittest.TestCase):
 
     def test_package(self):
         run_test("test_package")
-    
+
     def test_not_a_Unit(self):
         with self.assertRaises(AssertionError):
             run_test("test_not_a_Unit")
@@ -91,7 +98,7 @@ class HwtSchematic_directive_TC(unittest.TestCase):
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    # suite.addTest(HwtSchematic_directive_TC('test0'))
+    # suite.addTest(HwtSchematic_directive_TC('test_not_a_Unit'))
     suite.addTest(unittest.makeSuite(HwtSchematic_directive_TC))
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
