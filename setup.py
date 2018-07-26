bdb584e3c2fd1abd5c7fc85f6f877bfef87344a3
Nic30 <nic30@seznam.cz>
2018-07-26 19:26:52 +0200

    custom download of npm packages

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# from distutils.command.install_data import install_data
 from setuptools import find_packages, setup
 
 
@@ -9,33 +10,22 @@ def read(filename):
         return fp.read().strip()
 
 
-# javascript dependencies (npm)
-package_json = {
-    "dependencies": {
-        "d3-hwschematic": ">=0.0.3", # schematic visualizations
-    }
-}
-
 setup(
     name='sphinx-hwt',
     version='0.1',
     author="Michal Orsak",
     author_email="michal.o.socials@gmail.com",
     description="Sphinx extension to produce interactive schematic for hardware writen in HWT",
-    license='BSD',
+    license='BSD-3-Clause',
     keywords='sphinx documentation HWT FPGA hardware VHDL System Verilog schematic wave',
     url='https://github.com/Nic30/sphinx-hwt',
     packages=find_packages(exclude=['tests*']),
     long_description=read('README.md'),
     long_description_content_type="text/markdown",
     install_requires=[
-        'Sphinx>=1.7.6', # base sphinx doc generator
-        'hwtGraph>=0.0.1', # converts HWT Units to schemes
+        'Sphinx>=1.7.6',  # base sphinx doc generator
+        'hwtGraph>=0.0.1',  # converts HWT Units to schemes
     ],
-    setup_requires=[
-        'calmjs', # allows to import npm packages for visualization
-    ],
-    package_json=package_json,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
@@ -45,5 +35,14 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
     ],
+    entry_points={
+    "setuptools.file_finders": [
+        "foobar = sphinx_hwt._js_install:find_extra_js_files_in_npm"
+    ]},
+
+    package_data={
+        'sphinx_hwt': ['*.html', '*.css', "*.js"]
+    },
     include_package_data=True,
+    zip_safe=False,
 )
--- sphinx_hwt/__init__.py
+++ sphinx_hwt/__init__.py
@@ -1,1 +1,2 @@
+# setup of sphinx plugin
 from sphinx_hwt.sphinx_hwt import setup 
