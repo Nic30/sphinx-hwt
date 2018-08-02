66833f4d9ce8261e91ed221705c19ef80515d4c6
Nic30 <nic30@seznam.cz>
2018-08-02 10:29:10 +0200

    v0.6

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ class clean(_clean):
 npm_installation_check()
 setup(
     name='sphinx-hwt',
-    version='0.5',
+    version='0.6',
     author="Michal Orsak",
     author_email="michal.o.socials@gmail.com",
     description="Sphinx extension to produce interactive schematic for hardware writen in HWT",
