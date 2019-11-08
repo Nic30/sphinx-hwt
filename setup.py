04e8fb129e36d96198461329eba5250dc81c7ff3
Nic30 <nic30@seznam.cz>
2019-11-08 15:29:08 +0100

    v1.9

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ class clean(_clean):
 
 setup(
     name='sphinx-hwt',
-    version='1.8',
+    version='1.9',
     author="Michal Orsak",
     author_email="michal.o.socials@gmail.com",
     description="Sphinx extension to produce interactive schematic for hardware writen in HWT",
