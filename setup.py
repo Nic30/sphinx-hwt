7cff1cd5c77e82573901d2171fed8058d9c0f84d
Nic30 <nic30@seznam.cz>
2020-03-25 17:30:43 +0100

    update dep. sphinx>=2.3.1

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ setup(
     long_description=read('README.md'),
     long_description_content_type="text/markdown",
     install_requires=[
-        'sphinx>=2.2.1',  # base sphinx doc generator
+        'sphinx>=2.3.1',  # base sphinx doc generator
         'hwtGraph>=1.3',  # converts HWT Units to schematics
     ],
     classifiers=[
