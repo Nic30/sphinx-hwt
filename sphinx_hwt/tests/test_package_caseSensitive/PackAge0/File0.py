9f07ee2daacaf192165239a605b2b84700b1e143
Nic30 <nic30@seznam.cz>
2020-05-04 10:48:05 +0200

    add test_package_caseSensitive

---
+++ sphinx_hwt/tests/test_package_caseSensitive/PackAge0/File0.py
@@ -0,0 +1,16 @@
+from hwt.synthesizer.unit import Unit
+from hwt.interfaces.std import Signal
+
+
+class ExampleCls0(Unit):
+    """
+    .. hwt-schematic::
+
+    """
+
+    def _declr(self):
+        self.din = Signal()
+        self.dout = Signal()._m()
+
+    def _impl(self):
+        self.dout(self.din)
